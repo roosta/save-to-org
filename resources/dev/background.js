eafdb1b63767d5d11a21688d8dc9e8db0aa5e640
Daniel Berg <mail@roosta.sh>
2018-09-08 21:16:33 +0200

    Delete old manual files

--- resources/dev/background.js
+++
@@ -1,8 +0,0 @@
-var CLOSURE_UNCOMPILED_DEFINES = {"figwheel.repl.connect_url":"ws:\/\/localhost:9500\/figwheel-connect?fwprocess=dee8f9&fwbuild=background"};
-var CLOSURE_NO_DEPS = true;
-goog.require("figwheel.core");
-goog.require("figwheel.main");
-goog.require("figwheel.repl.preload");
-goog.require("figwheel.main.editor");
-goog.require("process.env");
-goog.require("yank.background");
